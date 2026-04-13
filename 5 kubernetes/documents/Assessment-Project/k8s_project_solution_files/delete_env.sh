3e5b2c48746020bfed779358a820740a965f74e8
Pradeep Viswanathan <pradeep.viswa@gmail.com>
2026-04-14 00:19:36 +0530

    updated

--- 5 kubernetes/documents/Assessment-Project/k8s_project_solution_files/delete_env.sh
+++ 5 kubernetes/documents/Assessment-Project/k8s_project_solution_files/delete_env.sh
@@ -2,6 +2,6 @@ kubectl delete svc --all
 kubectl delete deploy --all
 kubectl delete pvc --all
 kubectl delete pv --all
-kubectl secret pv --all
+kubectl delete secret --all
 kubectl configmap pv --all
 
